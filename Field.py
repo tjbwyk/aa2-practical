f839e5d6758804b0847432bf8ce1bc7eadbafb77
boromir674 <boromir674@hotmail.com>
2015-05-12 16:26:19 +0200

    Field class updated and Player a little bit

--- Field.py
+++ Field.py
@@ -1,28 +1,81 @@
 __author__ = 'kostas'
 
+from Player import Player
+from State import *
+from random import uniform
+
 
 class Field(object):
     """
-    Models the environment:
-    Responsibilities:
-    - Maintaining a list of agents
-    - coordination of the steps in an episode
-      - triggering the agents to pick their next action
-      - calculate next state and rewards based on actions
-      - return
+
     """
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, state_type=RelativeDistanceState()):
         self.width = width
         self.height = height
-        self.players = []
+        self.state = state_type
+        self.predators = []
+        self.prey = None
         self.steps = 0
 
     def add_player(self, player):
 
-        self.players.append(player)
+        if isinstance(player, Player):
+            self.predators.append(player)
+        else:
+            self.prey = player
 
     def get_current_state(self):
+        res = []
+        if self.state.dim == 2:
+            diff_x = self.predators[0].x - self.prey.x
+            diff_y = self.predators[0].y - self.prey.y
+            res = diff_x, diff_y
+        elif self.state.dim == 1:
+            diff_x = (self.predators[0].x - self.prey.x)**2
+            diff_y = (self.predators[0].y - self.prey.y)**2
+            res = math.sqrt(diff_x + diff_y)
 
-        diff_x = self.players(0).x - self.players(1).x
-        diff_y = self.players(0).y - self.players(1).y
+        return res
+
+    def game_over(self):
+        """
+        True if the prey is within a radius of 1 from the predator
+        :return: boolean
+        """
+        dist = 0
+        state = self.get_current_state()
+        if self.state.dim == 2:
+            dist = sum(state**2)
+        elif self.state.dim == 1:
+            dist = state
+
+        return dist < 1
+
+    def generate(self, m):
+        """
+        Uniformly samples #m states from S
+        :param m: number of states to sample
+        :return: list of states sampled
+        """
+        samples = []
+        boundary_x = self.width / 2.0
+        boundary_y = self.length / 2.0
+
+        if self.state.dim == 1:
+            for i in range(0, m):
+                rx = uniform(0, boundary_x)
+                ry = uniform(0, boundary_y)
+
+                current_sample = math.sqrt(rx**2 + ry**2)
+                samples.append(current_sample)
+
+        elif self.state.dim == 2:
+            for i in range(0, m):
+                rx = uniform(- boundary_x, boundary_x)
+                ry = uniform(- boundary_y, boundary_y)
+
+                current_sample = np.array([rx, ry])
+                samples.append(current_sample)
+
+        return samples
