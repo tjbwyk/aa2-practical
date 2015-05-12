c7af602c820e2a136f65c27c0df045db94c56332
Yikang Wang <tjbwyk@gmail.com>
2015-05-12 14:41:33 +0200

    add defination for Sampling-based Fitted Value Iteration

---
+++ model/SamplingBasedFittedValueIteration.py
@@ -0,0 +1,18 @@
+__author__ = 'Tommy'
+
+class SamplingBasedFittedValueIteration(object):
+    """
+    The Sampling-based Fitted Value Iteration algorithm, approximating the value function of the states.
+    """
+
+    def __init__(self, players, env):
+        pass
+
+    def fit(self):
+        pass
+
+    def getValue(self, state):
+        pass
+
+    def getActions(self, state):
+        pass
