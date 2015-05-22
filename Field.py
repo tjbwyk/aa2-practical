8306273162c698b9cac127f991c93805257449e9
boromir674 <boromir674@hotmail.com>
2015-05-22 09:50:54 +0200

    added run_n_times fuction in field

--- Field.py
+++ Field.py
@@ -90,27 +90,12 @@ class Field(object):
         print "%0.2f, %0.2f | %0.2f, %0.2f -- %0.2f"\
               % (self.predators[0].x, self.predators[0].y, self.prey.x, self.prey.y, d)
 
-    def run(self):
+    def get_reward(self, state):
         """
-        Runs one simulation of the world
+        The R (Reward) function
+        :param state: 
         :return:
         """
-        steps = 0
-        self.printme()
-        start_time = time.time()
-
-        while not self.game_over():
-            steps += 1
-            self.predators[0].move()
-            self.prey.move()
-            self.printme()
-
-        duration = time.time() - start_time
-        print "Steps:", steps
-        print "Duration:", duration
-
-    def get_reward(self, state):
-
         dist = 0
         if self.state.dim == 2:
             dist = math.sqrt(sum(state**2))
@@ -138,25 +123,68 @@ class Field(object):
         :param prey_loc: location of the prey
         :return: np.array [1x2] or [1x1]
         """
-        res1 = abs(pred_loc[0] - prey_loc[0])
+        res1 = pred_loc[0] - prey_loc[0]
         if res1 > (self.width / 2.0):
-            res1 = pred_loc[0] - prey_loc[0]
-            if res1 < 0:
-                res1 %= (self.width / 2.0)
-            else:
-                res1 = self.width - pred_loc[0] + prey_loc[0]
+            res1 = self.width - pred_loc[0] + prey_loc[0]
+        elif res1 < -(self.width / 2.0):
+            res1 = pred_loc[0] + self.width - prey_loc[0]
 
-        res2 = abs(pred_loc[1] - prey_loc[1])
+        res2 = pred_loc[1] - prey_loc[1]
         if res2 > (self.height / 2.0):
-            res2 = pred_loc[1] - prey_loc[1]
-            if res2 < 0:
-                res2 %= (self.height / 2.0)
-            else:
-                res2 = (-res2) % (self.height / 2.0)
+            res2 = self.height - pred_loc[1] + prey_loc[1]
+        elif res2 < -(self.height / 2.0):
+            res1 = pred_loc[1] + self.height - prey_loc[1]
 
         if self.state.dim == 1:
             result = np.array([math.sqrt(res1**2 + res2**2)])
         elif self.state.dim == 2:
             result = np.array([res1, res2])
-
         return result
+
+    def run(self):
+        """
+        Runs one simulation of the world
+        :return:
+        """
+        steps = 0
+        self.printme()
+        start_time = time.time()
+
+        while not self.game_over():
+            steps += 1
+            self.predators[0].move()
+            self.prey.move()
+            self.printme()
+
+        duration = time.time() - start_time
+        print "Steps:", steps
+        print "Duration:", duration
+
+    def run_n_times(self, n):
+        """
+        Runs the simulation n times and returns the average steps and time needed for the predator to catch the prey
+        :param n: number of times to run the simulation
+        :return: average_steps_per_episode, average_duration_per_episode
+        """
+        avg_steps = 0
+        avg_time = 0
+        for i in range(n):
+
+            steps = 0
+            start_time = time.time()
+
+            while not self.game_over():
+                steps += 1
+                self.predators[0].move()
+                self.prey.move()
+
+            duration = time.time() - start_time
+            avg_steps += steps
+            avg_time += duration
+
+            self.predators[0].x, self.predators[0].y = 0, 0
+            self.prey.x, self.prey.y = 5, 5
+
+        avg_steps /= n
+        avg_time /= n
+        return avg_steps, avg_time
